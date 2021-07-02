8b1c26f42d0b3dd42dbbc2c17aafa8623dda3d98
Facebamm <dbaumertffg@web.de>
2021-07-02 15:24:35 +0200

    Update

--- js/sketch.js
+++ js/sketch.js
@@ -7,41 +7,68 @@ let _hideGrayScale;
 let _hideSobelFilter;
 
 // Sliders
-let _sliderFilterX;
-let _sliderFilterY;
-let _sliderFilterXY;
-let _sliderFilterYX;
+let _sliderFilterL2R;
+let _sliderFilterR2L;
+let _sliderFilterT2B;
+let _sliderFilterB2T;
+let _sliderFilterTL2BR;
+let _sliderFilterBR2TL;
+let _sliderFilterBL2TR;
+let _sliderFilterTR2BL;
 
 // checkboxes
-let _hideFilterX;
-let _hideFilterY;
-let _hideFilterXY;
-let _hideFilterYX;
+let _hideFilterL2R;
+let _hideFilterR2L;
+let _hideFilterT2B;
+let _hideFilterB2T;
+let _hideFilterTL2BR;
+let _hideFilterBR2TL;
+let _hideFilterBL2TR;
+let _hideFilterTR2BL;
 
 const IGNORE = -1;
-const X_CANNY = [
+// filter
+const CANNY_L2R = [
 	[-1,  0,  1],
 	[-2,  0,  2],
 	[-1,  0,  1]
 ];
-
-const Y_CANNY = [
+const CANNY_R2L = [
+	[1,  0,  -1],
+	[2,  0,  -2],
+	[1,  0,  -1]
+];
+const CANNY_T2B= [
 	[-1, -2, -1],
 	[ 0,  0,  0],
 	[ 1,  2,  1]
 ];
-
-const XY_CANNY = [
+const CANNY_B2T = [
+	[ 1,  2,  1],
+	[ 0,  0,  0],
+	[-1, -2, -1]
+];
+const CANNY_TL2BR = [
 	[-2, -1,  0],
 	[-1,  0,  1],
 	[ 0,  1,  2]
 ];
-
-const YX_CANNY = [
+const CANNY_BR2TL = [
+	[ 2,  1,  0],
+	[ 1,  0, -1],
+	[ 0, -1, -2]
+];
+const CANNY_BL2TR = [
 	[ 0,  1,  2],
 	[-1,  0,  1],
 	[-2, -1,  0]
 ];
+const CANNY_TR2BL = [
+	[ 0, -1, -2],
+	[ 1,  0, -1],
+	[ 2,  1,  0]
+];
+
 
 function preload() {
 	_img = loadImage('assets/buntes.jpg');
@@ -73,36 +100,68 @@ function setup() {
 		function() { _hideSobelFilter = this.checked(); } 		// changed function
 	);
 
-	_sliderFilterX = createSliderEx(
-		"Canny filter in x axis right", 						// title
-		610, uiOffset += 20, 									// x, y 
+	_sliderFilterL2R = createSliderEx(
+		"Canny filter from left to right", 						// title
+		610, uiOffset += 40, 									// x, y 
 		100, IGNORE, 											// width, height
 		0, 60, 30, 												// min, max, value
-		function(){ _hideFilterX = this.checked(); }			// changed function
+		function(){ _hideFilterL2R = this.checked(); }			// changed function
 	);
 
-	_sliderFilterY = createSliderEx(
-		"Canny filter in y axis right", 						// title
+	_sliderFilterR2L = createSliderEx(
+		"Canny filter from right to letf", 						// title
+		610, uiOffset += 40, 									// x, y 
+		100, IGNORE, 											// width, height
+		0, 60, 30, 												// min, max, value
+		function(){ _hideFilterR2L = this.checked(); }			// changed function
+	);
+	
+	_sliderFilterT2B = createSliderEx(
+		"Canny filter from top to bottom", 						// title
 		610, uiOffset += 40,									// x, y 
 		100, IGNORE, 											// width, height
 		0, 60, 30, 												// min, max, value
-		function(){ _hideFilterY = this.checked(); }			// changed function	
+		function(){ _hideFilterT2B = this.checked(); }			// changed function	
 	);
 	
-	_sliderFilterXY = createSliderEx(
-		"Canny filter in xy axis right",				 		// title
+	_sliderFilterB2T = createSliderEx(
+		"Canny filter from bottom to top", 						// title
+		610, uiOffset += 40,									// x, y 
+		100, IGNORE, 											// width, height
+		0, 60, 30, 												// min, max, value
+		function(){ _hideFilterB2T = this.checked(); }			// changed function	
+	);
+
+	_sliderFilterTL2BR = createSliderEx(
+		"Canny filter from top left to bottom right", 			// title
 		610,  uiOffset += 40, 									// x, y 
 		100, IGNORE, 											// width, height
 		0, 60, 30, 												// min, max, value
-		function(){ _hideFilterXY = this.checked();}			// changed function	
+		function(){ _hideFilterTL2BR = this.checked();}			// changed function	
+	);
+
+	_sliderFilterBR2TL = createSliderEx(
+		"Canny filter from bottom right to top left", 			// title
+		610,  uiOffset += 40, 									// x, y 
+		100, IGNORE, 											// width, height
+		0, 60, 30, 												// min, max, value
+		function(){ _hideFilterBR2TL = this.checked();}			// changed function	
 	);
 	
-	_sliderFilterYX = createSliderEx(
-		"Canny filter in yx axis right",						// title
+	_sliderFilterBL2TR = createSliderEx(
+		"Canny filter bottom left to top right",				// title
 		610, uiOffset += 40, 									// x, y
 		100, IGNORE, 											// width, height
 		0, 60, 30, 												// min, max, value
-		function(){ _hideFilterYX = this.checked();}			// changed function
+		function(){ _hideFilterBL2TR = this.checked();}			// changed function
+	);
+
+	_sliderFilterTR2BL = createSliderEx(
+		"Canny filter top right to bottom left",				// title
+		610, uiOffset += 40, 									// x, y
+		100, IGNORE, 											// width, height
+		0, 60, 30, 												// min, max, value
+		function(){ _hideFilterTR2BL = this.checked();}			// changed function
 	);
 
 	pixelDensity(1);
@@ -123,23 +182,27 @@ function createSliderEx(title, x, y, width, height, min, max, value, changedFunc
 
 	return slider;
 }
+
 function createCheckBoxEx(title, x, y, status, changedFunc) { 
 	let checkbox = createCheckbox(title, status);
 	checkbox.position(x, y);
 	checkbox.changed(changedFunc);
 }
+
 function draw() {
 	clear();
 
 	// Slider values as filter
-	let sliderFilterXValue = _sliderFilterX.value();
-	let sliderFilterYvalue = _sliderFilterY.value();
-	let sliderFilterXYValue = _sliderFilterXY.value();
-	let sliderFilterYXvalue = _sliderFilterYX.value();
+	let sliderFilterL2RValue = _sliderFilterL2R.value();
+	let sliderFilterR2LValue = _sliderFilterR2L.value();
+	let sliderFilterT2Bvalue = _sliderFilterT2B.value();
+	let sliderFilterB2Tvalue = _sliderFilterB2T.value();
+	let sliderFilterTL2BRValue = _sliderFilterTL2BR.value();
+	let sliderFilterBR2TLValue = _sliderFilterBR2TL.value();
+	let sliderFilterBL2TRvalue = _sliderFilterBL2TR.value();
+	let sliderFilterTR2BLvalue = _sliderFilterTR2BL.value();
 
 	loadPixels();
-
-
 
 	for (let x = 0; x < _img.width; x += 1) {
 		for (let y = 0; y < _img.height; y += 1) {
@@ -184,31 +247,44 @@ function draw() {
 			for (let y = 1; y < _img.height - 1; y += 1) {
 				// Calculate the 1D location from a 2D grid
 				let loc = (x + y * _img.width) * 4;
+			
+
 				// canny
-				let kernelX = 0;
-				let kernelY = 0;
-				
-				let kernelXY = 0;
-				let kernelYX = 0;
+				let kernelL2R = 0;
+				let kernelR2L = 0;
+				let kernelT2B = 0;
+				let kernelB2T = 0;
+				let kernelTL2BR = 0;
+				let kernelBR2TL = 0;
+				let kernelBL2TR = 0;
+				let kernelTR2BL = 0;
+
 				// for([start variablen]; [abbruch bedingdung]; [schritte]) {[code]}
 				for (let matrixX = -1; matrixX < 2; matrixX += 1) {
 					for (let matrixY = -1; matrixY < 2; matrixY += 1) {
 						// Calculate the 1D location from a 2D grid
 						let imgLoc = ((x + matrixX) + (y + matrixY) * _img.width) * 4;
 						// Get the value form the Kernel
-						let xCannyValue = X_CANNY[1 + matrixY][1 + matrixX];
-						let yCannyValue = Y_CANNY[1 + matrixY][1 + matrixX];
+						let cannyValueL2R = CANNY_L2R[1 + matrixY][1 + matrixX];
+						let cannyValueR2L = CANNY_R2L[1 + matrixY][1 + matrixX];
+						let cannyValueT2B = CANNY_T2B[1 + matrixY][1 + matrixX];
+						let cannyValueB2T = CANNY_B2T[1 + matrixY][1 + matrixX];
+						let cannyValueTL2BR = CANNY_TL2BR[1 + matrixY][1 + matrixX];
+						let cannyValueBR2TL = CANNY_BR2TL[1 + matrixY][1 + matrixX];
+						let cannyValueBL2TR = CANNY_BL2TR[1 + matrixY][1 + matrixX];
+						let cannyValueTR2BL = CANNY_TR2BL[1 + matrixY][1 + matrixX];
 						
-						let xyCannyValue = XY_CANNY[1 + matrixY][1 + matrixX];
-						let yxCannyValue = YX_CANNY[1 + matrixY][1 + matrixX];
 						// Get the R,G,B values from image
 						let grayValue = _grayScaledImg[imgLoc + 0];
 
-						kernelX += xCannyValue * grayValue;
-						kernelY += yCannyValue * grayValue;
-
-						kernelXY += xyCannyValue * grayValue;
-						kernelYX += yxCannyValue * grayValue;
+						kernelL2R += cannyValueL2R * grayValue;
+						kernelR2L += cannyValueR2L * grayValue;
+						kernelT2B += cannyValueT2B * grayValue;
+						kernelB2T += cannyValueB2T * grayValue;
+						kernelTL2BR += cannyValueTL2BR * grayValue;
+						kernelBR2TL += cannyValueBR2TL * grayValue;
+						kernelBL2TR += cannyValueBL2TR * grayValue;
+						kernelTR2BL += cannyValueTR2BL * grayValue;	
 					}
 				}
 
@@ -216,30 +292,54 @@ function draw() {
 				
 				// Make a new color and set pixel in the window
 				//Filter
-				if (!_hideFilterX & kernelX > sliderFilterXValue) {
+				
+				if (!_hideFilterL2R & kernelL2R > sliderFilterL2RValue){ 
 					pixels[loc + 0] = 255;
 					pixels[loc + 1] = 0;
 					pixels[loc + 2] = 0;
-					pixels[loc + 3] = 200;
+					pixels[loc + 3] = 200; // transparency
 				}
-				
-				if (!_hideFilterY & kernelY > sliderFilterYvalue) {
-					pixels[loc + 0] = 0;
-					pixels[loc + 1] = 0;
-					pixels[loc + 2] = 255;
-					pixels[loc + 3] = 200;
+				if (!_hideFilterR2L & kernelR2L > sliderFilterR2LValue){ 
+					pixels[loc + 0] = 255;
+					pixels[loc + 1] = 255;
+					pixels[loc + 2] = 0;
+					pixels[loc + 3] = 200; // transparency
 				}
-				if (!_hideFilterXY & kernelXY > sliderFilterXYValue) {
+				if (!_hideFilterT2B & kernelT2B > sliderFilterT2Bvalue){ 
 					pixels[loc + 0] = 0;
 					pixels[loc + 1] = 255;
 					pixels[loc + 2] = 0;
-					pixels[loc + 3] = 200;
+					pixels[loc + 3] = 200; // transparency
 				}
-				if (!_hideFilterYX & kernelYX > sliderFilterYXvalue) {
+				if (!_hideFilterB2T & kernelB2T > sliderFilterB2Tvalue){ 
+					pixels[loc + 0] = 0;
+					pixels[loc + 1] = 255;
+					pixels[loc + 2] = 255;
+					pixels[loc + 3] = 200; // transparency
+				}
+				if (!_hideFilterTL2BR & kernelTL2BR > sliderFilterTL2BRValue){ 
+					pixels[loc + 0] = 0;
+					pixels[loc + 1] = 0;
+					pixels[loc + 2] = 255;
+					pixels[loc + 3] = 200; // transparency
+				}
+				if (!_hideFilterBR2TL & kernelBR2TL > sliderFilterBR2TLValue){ 
 					pixels[loc + 0] = 255;
 					pixels[loc + 1] = 0;
 					pixels[loc + 2] = 255;
-					pixels[loc + 3] = 200;
+					pixels[loc + 3] = 200; // transparency
+				}
+				if (!_hideFilterBL2TR & kernelBL2TR > sliderFilterBL2TRvalue){ 
+					pixels[loc + 0] = 255;
+					pixels[loc + 1] = 125;
+					pixels[loc + 2] = 255;
+					pixels[loc + 3] = 200; // transparency
+				}
+				if (!_hideFilterTR2BL & kernelTR2BL > sliderFilterTR2BLvalue){ 
+					pixels[loc + 0] = 125;
+					pixels[loc + 1] = 255;
+					pixels[loc + 2] = 125;
+					pixels[loc + 3] = 200; // transparency
 				}
 			}
 		}
